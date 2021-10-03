1db43fb7bf8e6a08d84742c127b35ecea8a9e17b
jaweria332 <hiyamughal03@gmail.com>
2021-10-03 17:31:58 +0500

    Update image_to_text.py

--- image_to_text.py
+++ image_to_text.py
@@ -1,3 +1,13 @@
 # Importing necessary libraries
 from PIL import Image
 from pytesseract import *
+
+# Importing necessary libraries
+from PIL import Image
+from pytesseract import *
+
+pytesseract.tesseract_cmd = r'C:\Users\dell\AppData\Local\Programs\Tesseract-OCR\tesseract.exe'
+
+img = Image.open("input_img.png")
+output = pytesseract.image_to_string(img)
+print(output)
